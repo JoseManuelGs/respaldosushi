ce11982b8bbdea0f1f59ff48b98256dc529ee8a0
joseguerrerosimental <joseguerrerosimental@gmail.com>
2025-04-14 22:32:41 +0000

    firebase

--- screens/firebaseConfig.js
+++ screens/firebaseConfig.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app.js";
+import { getAuth } from "firebase/auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSG9VrMxIdb3sDd0eyKoB6LIsLuj-cURA",
